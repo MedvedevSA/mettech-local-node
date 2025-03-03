ebac55bece67406bcda725d0623d2c3c258861b0
medvedevsa <medvedevsa97@gmail.com>
2025-03-04 00:58:53 +0300

    main.go -> mettech explorer

--- curl.sh
+++
@@ -1,3 +0,0 @@
-curl -X POST localhost:2003/exec/explorer \
-    -H 'Content-Type: application/json' \
-    -d '{"path": "/home/sam/dev"}'
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module user-backend/main
+module user-backend/mettech-explorer
 
 go 1.24.0
 
